a834ba3142e7b489600c2c35c34acf068c24ecbf
Damien Fraud <damien.fraud@gmail.com>
2018-10-24 07:14:47 +0200

    :gift: Add new Xcode templates.

---
+++ Xcode Templates/User Interface.xctemplate/___FILEBASENAME___PresenterInput.swift
@@ -0,0 +1,15 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+import Foundation
+
+protocol ___FILEBASENAMEASIDENTIFIER___
+{
+    /// Notifies that the view did load
+    func viewDidLoad()
+}
